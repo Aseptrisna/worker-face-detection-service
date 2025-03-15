e2f65636822bbf88709bcdb51bf5a4bd0c5ac61c
asept <aseptrisna1995@gmail.com>
2025-03-16 00:48:06 +0700

    first commit

---
+++ ecosystem.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    apps: [
+      {
+        name: "ai-image-processor", // Nama aplikasi
+        script: "python3",          // Perintah untuk menjalankan Python
+        args: "main.py",            // File Python yang akan dijalankan
+        interpreter: "",            // Biarkan kosong untuk menggunakan Python default
+        env: {
+          NODE_ENV: "production",
+        },
+        log_date_format: "YYYY-MM-DD HH:mm:ss",
+        error_file: "logs/error.log", // File log untuk error
+        out_file: "logs/out.log",     // File log untuk output
+        merge_logs: true,
+        autorestart: true,            // Restart otomatis jika crash
+        watch: false,                 // Jangan pantau perubahan file
+        max_memory_restart: "1G",     // Restart jika penggunaan memori melebihi 1GB
+      },
+    ],
+  };
